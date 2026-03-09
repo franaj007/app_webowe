10d985e065c1449328323de652b13510ce3a7693
Franciszek Najdek <najdek.franciszek@gmail.com>
2026-03-09 14:17:40 +0100

    added .idea

--- 2026.02.02/src/components/Footer/x.jsx
+++
@@ -1,9 +0,0 @@
-import {useState} from "react";
-
-export function X () {
-    const [state, setState] = useState({});
-    setState(prevState => {
-        // Object.assign również zadziała
-        return {...prevState, ...updatedValues};
-    });
-}
